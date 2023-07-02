44bbe00b2e40be0131ab6ee84effe623d8e25b49
Tin26522 <106314906+Tin26522@users.noreply.github.com>
2023-07-02 21:45:01 +0700

    Update Program.cs

---
+++ Csharpbasic/Csharpbasic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharpbasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Nhập Tên Của Bạn: ");
+            string hoten = Console.ReadLine();
+            Console.WriteLine("Tên của bạn là " + hoten) ;
+
+        }
+    }
+}
