cac6df3cb317758b0b8d6e95fcf3ef7d0933a62b
Tin26522 <106314906+Tin26522@users.noreply.github.com>
2023-07-05 09:57:23 +0700

    Update CGO_chuyendoitiente.cs

---
+++ Csharpbasic/Bai2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập ngày sinh của bạn :");
+            string input = Console.ReadLine();
+
+            DateTime birthday;
+            if (DateTime.TryParseExact(input, "yyyy/MM/dd", null, System.Globalization.DateTimeStyles.None, out birthday))
+            {
+                DateTime today = DateTime.Today;
+
+                if (today.Month == birthday.Month && today.Day == birthday.Day)
+                {
+                    Console.WriteLine("Chúc mừng sinh nhật!");
+                }
+                else
+                {
+                    DateTime nextBirthday = new DateTime(today.Year, birthday.Month, birthday.Day);
+                    if (nextBirthday < today)
+                    {
+                        nextBirthday = nextBirthday.AddYears(1);
+                    }
+
+                    int daysUntilBirthday = (nextBirthday - today).Days;
+                    Console.WriteLine("Còn {0} ngày nữa là tới sinh nhật của bạn.", daysUntilBirthday);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ngày sinh không hợp lệ.");
+            }
+        }
+    }
+}
---
+++ Csharpbasic/CGO_chuyendoitiente/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_chuyendoitiente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+    {
+        double rate = 23000;
+
+        Console.Write(" Nhap gia tri USD: ");
+        double usdAmount = double.Parse(Console.ReadLine());
+
+        double vndAmount = usdAmount * rate;
+
+        Console.WriteLine(" Gia tri tuong ung VND: " + vndAmount);
+    }
+}
+}
