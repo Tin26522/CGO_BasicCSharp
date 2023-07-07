4f0471e38bccd3904d50c50832cc6300f403c822
Tin26522 <106314906+Tin26522@users.noreply.github.com>
2023-07-07 23:00:40 +0700

    update Phuongtrinhbacnhat.cs

---
+++ Csharpbasic/Phuongtrinhbacnhat/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phuongtrinhbacnhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Nhap a va b");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            if (a != 0)
+            {
+                double solution = -b / a;
+                Console.WriteLine("Ket qua la: {0}!", solution);
+
+            }
+            else
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("phuong trinh vo so nghiem");
+                }
+                else
+                {
+                    Console.WriteLine("phuong trinh vo nhghiem");
+                }
+            }
+        }
+    }
+}
---
+++ Csharpbasic/bangcuuchuong/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bangcuuchuong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 10; i++)
+            {
+                Console.WriteLine("Bảng cửu chương :", + i);
+
+                for (int j = 1; j <= 10; j++)
+                {
+                    int result = i * j;
+                    Console.WriteLine("{0} x {1} = {2}", i, j, result);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+    }
+
