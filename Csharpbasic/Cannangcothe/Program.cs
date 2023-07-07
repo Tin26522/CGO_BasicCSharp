46ba88dfa47e760e5b38a4c84d81640bad1d8834
Tin26522 <106314906+Tin26522@users.noreply.github.com>
2023-07-07 23:22:19 +0700

    update Cannangcothe.cs

---
+++ Csharpbasic/Cannangcothe/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cannangcothe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double height;
+            double weight;
+            Console.WriteLine("Nhap chieu cao cua ban : ");
+            height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap can nang cua ban : ");
+            weight = float.Parse(Console.ReadLine());
+            //tinh bmi
+            double bmi = weight / Math.Pow(height, 2);
+            bmi = Math.Round(bmi, 1);
+            //hien thi 
+
+            Console.Write("BMI: " + bmi);
+
+            if (bmi < 18)
+                Console.WriteLine(" Underweight");
+            else if (bmi < 25.0)
+                Console.WriteLine(" Normal");
+            else if (bmi < 30.0)
+                Console.WriteLine(" Overweight");
+            else
+                Console.WriteLine(" Obese");
+        }
+    }
+}
