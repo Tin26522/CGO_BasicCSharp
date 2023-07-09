c18a41782fe8baccc28061ec64e82d3c0b3c19b4
Tin26522 <106314906+Tin26522@users.noreply.github.com>
2023-07-09 18:06:11 +0700

    update tinhtongmang.cs

---
+++ Csharpbasic/tinhtongmang/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tinhtongmang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+
+            numbers[0] = 2;
+            numbers[1] = 5;
+            numbers[2] = 9;
+            numbers[3] = 6;
+            numbers[4] = 7;
+            int S = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                S = S + numbers[i];
+            }
+            Console.WriteLine(S);
+        }
+    }
+}
